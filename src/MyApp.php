171a72c44a654bfa5a85dde6427f578d09839246
bastien-jcln <bastien.jcln@eduge.ch>
2026-09-15 11:26:23 +0200

    Extract an app class of index to be easier to reach with pest

---
+++ src/MyApp.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BastienJcln\SwissCooking;
+
+use Slim\App;
+use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpInternalServerErrorException;
+
+class MyApp
+{
+    public static function create(): App
+    {
+        $app = AppFactory::create();
+
+        $errorMiddleware = $app->addErrorMiddleware(
+            false,
+            true,
+            true
+        );
+
+        $errorMiddleware->setErrorHandler(
+            HttpNotFoundException::class,
+            function ($request, $exception, $displayErrorDetails) {
+                $response = new \Slim\Psr7\Response();
+
+                $view = new PhpRenderer(
+                    __DIR__ . '/../views'
+                );
+
+                $view->setLayout('layout.php');
+
+                return $view->render(
+                    $response->withStatus(404),
+                    'errors/404.php',
+                    [
+                        'withMenu' => false,
+                        'title' => 'Page non trouvée',
+                        'message' => $exception->getMessage(),
+                    ]
+                );
+            }
+        );
+
+        $errorMiddleware->setErrorHandler(
+            HttpInternalServerErrorException::class,
+            function ($request, $exception, $displayErrorDetails) {
+                $response = new \Slim\Psr7\Response();
+
+                $view = new PhpRenderer(
+                    __DIR__ . '/../views'
+                );
+
+                $view->setLayout('layout.php');
+
+                return $view->render(
+                    $response->withStatus(500),
+                    'errors/500.php',
+                    [
+                        'withMenu' => false,
+                        'title' => 'Erreur interne du serveur',
+                        'message' => $exception->getMessage(),
+                    ]
+                );
+            }
+        );
+
+        require __DIR__ . '/../routes/web.php';
+
+        return $app;
+    }
+}
